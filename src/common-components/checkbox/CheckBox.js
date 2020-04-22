9691951be9ccac152e6f943dd65154c054d121e1
Caio <caiookb@gmail.com>
2020-04-22 00:26:54 -0300

    feature(Checkbox): Created checkbox toggle component

---
+++ src/common-components/checkbox/CheckBox.js
@@ -0,0 +1,18 @@
+import React, {Component, version} from 'react';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+import styles from './styles';
+import {toggleOff, toggleOn} from '../../assets/images';
+
+const CheckBox = props => {
+  const {enable, onPress, title} = props;
+  return (
+    <View style={styles.toggle}>
+      <Text style={styles.CheckBoxTitle}> {title} </Text>
+      <TouchableOpacity onPress={onPress}>
+        <Image source={enable ? toggleOn : toggleOff} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CheckBox;
