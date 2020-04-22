49372f538333f4ff014010dc5e36304cbf9396fe
Caio <caiookb@gmail.com>
2020-04-22 00:25:34 -0300

    refactor(Buttons): Refactored button common component

--- src/common-components/buttons/buttons.js
+++ src/common-components/buttons/buttons.js
@@ -1,15 +1,41 @@
 import React, {Component, version} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import styles from './styles';
+import {Colors} from '../../assets/colors';
 
-const CustomButton = props => {
-  const {width, height, color, onPress, onLongPress, disabled, title} = props;
+export const CustomButton = props => {
+  const {color, onPress, title, type} = props;
+  return (
+    <View style={[styles.button]}>
+      <TouchableOpacity
+        onPress={onPress}
+        style={[
+          styles.roundedButton,
+          {backgroundColor: color},
+          type === 'single' ? {marginBottom: 15} : null,
+        ]}>
+        <Text style={styles.buttonsText}> {title} </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export const IconButton = props => {
+  const {title, icon, color, onPress, type} = props;
 
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={[styles.buttonsConfirm, {backgroundColor: color}]}>
-      <Text style={styles.buttonsText}> {title} </Text>
+      style={[
+        styles.IconButton,
+        {backgroundColor: color},
+        type === 'spendings' ? styles.IconButtonSpending : null,
+      ]}>
+      <Image
+        style={type === 'spendings' ? styles.rotateIconSpending : null}
+        source={icon}
+      />
+      <Text style={styles.IconButtonText}> {title} </Text>
     </TouchableOpacity>
   );
 };
@@ -18,7 +44,6 @@ CustomButton.defaultProps = {
   width: '75%',
   height: 35,
   rounded: false,
-  color: '#0F70B8',
   onPress: () => {},
   onLongPress: () => {},
   style: {},
@@ -26,5 +51,3 @@ CustomButton.defaultProps = {
   disabled: false,
   children: null,
 };
-
-export default CustomButton;
